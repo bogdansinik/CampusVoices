85d6a77f534b426f4bf4e7a3200dace9e29a779e
AndrejPer <andrej.perkovic@icloud.com>
2023-05-27 14:06:56 +0200

    login

--- login.php
+++ login.php
@@ -46,62 +46,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html>
 <head>
   <title>Campus Voices - Login</title>
+  <link rel="stylesheet" type="text/css" href="styleLogin.css">
   <style>
-    body {
-      background-color: lightblue;
-      font-family: Arial, sans-serif;
-    }
-    
-    #container {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-gap: 20px;
-      justify-items: center;
-      align-items: center;
-      height: 100vh;
-    }
-    
-    #login-form {
-      background-color: white;
-      padding: 30px;
-      border-radius: 5px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    }
-    
-    #login-form h2 {
-      text-align: center;
-      color: orange;
-    }
-    
-    #login-form input[type="text"],
-    #login-form input[type="password"] {
-      width: 100%;
-      padding: 10px;
-      margin-bottom: 15px;
-      border: 1px solid #ccc;
-      border-radius: 3px;
-    }
-    
-    #login-form button {
-      display: block;
-      width: 100%;
-      padding: 10px;
-      background-color: orange;
-      border: none;
-      border-radius: 3px;
-      color: white;
-      font-weight: bold;
-    }
-    
-    #login-form a {
-      text-align: center;
-      color: #777;
-      text-decoration: none;
-    }
-    
-    #login-form a:hover {
-      color: orange;
-    }
   </style>
 </head>
 <body>
@@ -111,12 +57,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </div>
     <div id="login-form">
       <h2>Campus Voices</h2>
-      <form>
+      <form action="login.php" method="post">
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
         <button type="submit">Sign In</button>
       </form>
-      <a href="#">Forgot password?</a>
+      <a href="register.php">Not registered?</a>
     </div>
   </div>
 </body>
