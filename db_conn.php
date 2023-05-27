f3a4437a0bbfc63290c0175a410dde412b5981d5
Bogdan Sinik <bogdansinik2001@gmail.com>
2023-05-28 00:00:58 +0200

    something

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
 <?php //CHANGE THE FOLLOWING WHEN MOVING TO SERVER ON FACULTY!!! VVVV
 $servername = '';
-$username = 'codeigniter';
-//$username = 'root';
-$password = 'codeigniter2019';
+//$username = 'codeigniter';
+$username = 'root';
+//$password = 'codeigniter2019';
 //$password = '';
 //$db_name = 'CV_proj';
 $db_name = 'CampusVoices';
@@ -10,6 +10,8 @@ $urlroot = 'https://www.studenti.famnit.upr.si/~89201013/CampusVoices/login.php'
 //$urlroot = "~/Documents/Faculty/3rd/%20year/MIT/CampusVoices/";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db_name);
+
+
 
 // Check connection
 if ($conn->connect_error) {
