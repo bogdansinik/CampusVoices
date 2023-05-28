29ff415171cb68bf94256d36c432caa57bc2f305
Bogdan Sinik <74776697+bogdansinik@users.noreply.github.com>
2023-05-28 10:54:56 +0200

    Fixed admin header.php

--- header.php
+++ header.php
@@ -9,31 +9,26 @@ $role = $_SESSION['role'];
 //echo $role;
 
 if($role == 'admin'){
-    echo '<header>
-        <h1>University of Primorska Campus Voices</h1>
-        <nav>
-            <ul class="top-row">
-                <li><a href="primorskaHome.php">Home</a></li>
-                <li><a href="primorskaCourses.php">Courses</a></li>
-                <li><a href="primorskaAccommodation.php">Accommodation</a></li>
-            </ul>
-            <ul class="bottom-row">
-                <li><a href="primorskaFood.php">Food</a></li>
-                <li><a href="primorskaProfessors.php">Professors</a></li>
-                <li><a href="primorskaFun.php">Fun</a></li>
-            </ul>
-            <ul class="logout">
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-        </nav>
-    </header>';
+    if($role == 'admin'){
+    echo "<header>";
+    echo "<h1>University of Primorska Campus Voices</h1>";
+    echo '<nav>
+    <ul>
+        <li><a href="adminCourses.php">Courses</a></li>
+        <li><a href="adminAccommodations.php">Accommodation</a></li>
+        <li><a href="adminFood.php">Food</a></li>
+        <li><a href="adminProfessors.php">Professors</a></li>
+        <li><a href="logout.php">Logout</a></li>';
+    echo ' </ul>
+    </nav>
+</header>';
 
 }else{
     echo '<header>
         <h1>University of Primorska Campus Voices</h1>
         <nav>
             <ul>
-                <li><a href="primorskaHome.php">Home</a></li>
+                <li><a href="welcomePage.php">Home</a></li>
                 <li><a href="primorskaCourses.php">Courses</a></li>
                 <li><a href="primorskaAccommodation.php">Accommodation</a></li>
                 <li><a href="primorskaFood.php">Food</a></li>
